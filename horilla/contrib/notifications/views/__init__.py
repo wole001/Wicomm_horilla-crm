7e3262768e3fcdd5e0695980160cca669705fe6a
Horilla <support@horilla.com>
2026-05-18 14:51:10 +0530

    [UPDT] NOTIFICATION: Refactored the notification __init__ with proper modularization

--- horilla/contrib/notifications/views/__init__.py
+++ horilla/contrib/notifications/views/__init__.py
@@ -1,4 +1,37 @@
 """Aggregate view modules for the `notifications.views` package."""
 
-from horilla.contrib.notifications.views.core import *
-from horilla.contrib.notifications.views.notification_template import *
+from horilla.contrib.notifications.views.core import (
+    MarkNotificationReadView,
+    MarkAllNotificationsReadView,
+    DeleteNotification,
+    DeleteAllNotification,
+    OpenNotificationView,
+    ToggleNotificationSoundView,
+)
+from horilla.contrib.notifications.views.notification_template import (
+    NotificationTemplateView,
+    NotificationTemplateNavbar,
+    NotificationTemplateListView,
+    NotificationTemplateCreateUpdateView,
+    NotificationTemplateDeleteView,
+    NotificationTemplateDetailView,
+    NotificationTemplateFieldSelectionView,
+)
+
+__all__ = [
+    # Core Views
+    "MarkNotificationReadView",
+    "MarkAllNotificationsReadView",
+    "DeleteNotification",
+    "DeleteAllNotification",
+    "OpenNotificationView",
+    "ToggleNotificationSoundView",
+    # Notification Template Views
+    "NotificationTemplateView",
+    "NotificationTemplateNavbar",
+    "NotificationTemplateListView",
+    "NotificationTemplateCreateUpdateView",
+    "NotificationTemplateDeleteView",
+    "NotificationTemplateDetailView",
+    "NotificationTemplateFieldSelectionView",
+]
