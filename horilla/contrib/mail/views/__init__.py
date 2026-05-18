cf49b6ccb8c1e8baeea41c354980bc7aa60187fc
Horilla <support@horilla.com>
2026-05-18 14:50:41 +0530

    [UPDT] MAIL: Refactored the mail __init__ with proper modularization

--- horilla/contrib/mail/views/__init__.py
+++ horilla/contrib/mail/views/__init__.py
@@ -1,7 +1,72 @@
 """Aggregate view modules for the  package."""
 
 from horilla.contrib.mail.views.core import *
-from horilla.contrib.mail.views.mail_template import *
-from horilla.contrib.mail.views.outlook import *
-from horilla.contrib.mail.views.incoming_mail import *
-from horilla.contrib.mail.views.outgoing_mail import *
+from horilla.contrib.mail.views.mail_template import (
+    MailTemplateView,
+    MailTemplateNavbar,
+    MailTemplateListView,
+    MailTemplateCreateUpdateView,
+    MailTemplatePreviewView,
+    TemplateContentView,
+    MailTemplateSelectView,
+    SaveAsMailTemplateView,
+    MailTemplateDeleteView,
+    MailTemplateDetailView,
+)
+from horilla.contrib.mail.views.outlook import (
+    OutlookMailServerFormView,
+    OutlookLoginView,
+    OutlookCallbackView,
+    refresh_outlook_token,
+    OutlookRefreshTokenView,
+)
+from horilla.contrib.mail.views.incoming_mail import (
+    IncomingMailServerView,
+    IncomingMailServerNavbar,
+    IncomingMailServerTypeSelectionView,
+    IncomingMailServerListView,
+    IncomingMailServerFormView,
+)
+from horilla.contrib.mail.views.outgoing_mail import (
+    MailServerView,
+    MailServerNavbar,
+    MailServerTypeSelectionView,
+    MailServerListView,
+    OutgoingMailServerFormView,
+    MailServerTestEmailView,
+    MailServerDeleteView,
+)
+
+__all__ = [
+    # Mail template views
+    "MailTemplateView",
+    "MailTemplateNavbar",
+    "MailTemplateListView",
+    "MailTemplateCreateUpdateView",
+    "MailTemplatePreviewView",
+    "TemplateContentView",
+    "MailTemplateSelectView",
+    "SaveAsMailTemplateView",
+    "MailTemplateDeleteView",
+    "MailTemplateDetailView",
+    # Outlook views
+    "OutlookMailServerFormView",
+    "OutlookLoginView",
+    "OutlookCallbackView",
+    "refresh_outlook_token",
+    "OutlookRefreshTokenView",
+    # Incoming mail server views
+    "IncomingMailServerView",
+    "IncomingMailServerNavbar",
+    "IncomingMailServerTypeSelectionView",
+    "IncomingMailServerListView",
+    "IncomingMailServerFormView",
+    # Outgoing mail views
+    "MailServerView",
+    "MailServerNavbar",
+    "MailServerTypeSelectionView",
+    "MailServerListView",
+    "OutgoingMailServerFormView",
+    "MailServerTestEmailView",
+    "MailServerDeleteView",
+]
