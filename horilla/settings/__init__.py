5c33dc851689c2bada634225afc4b4a78fa1797c
Horilla <support@horilla.com>
2026-06-20 01:16:06 +0700

    [FIX] SETTINGS: Apply local settings after app extensions
    Load `horilla.settings.local_settings` after `horilla.settings.horilla_apps`
    so local overrides are applied last and are not overwritten by app-level
    initialization side effects.

--- horilla/settings/__init__.py
+++ horilla/settings/__init__.py
@@ -1,5 +1,5 @@
 """Django settings aggregation for the Horilla project."""
 
 from horilla.settings.base import *
-from horilla.settings.local_settings import *
 from horilla.settings import horilla_apps
+from horilla.settings.local_settings import *
